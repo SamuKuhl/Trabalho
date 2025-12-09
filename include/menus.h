e976d79313d39aa65ee7cf7f05ff0a30c387ad81
SamuKuhl <samualveskuhl@gmail.com>
2025-12-09 10:59:24 -0300

    main

---
+++ include/menus.h
@@ -0,0 +1,9 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+int menu_principal();
+int menu_cadastros();
+int menu_relatorios();
+int menu_saida();
+
+#endif
---
+++ include/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+void remover_enter(char *str);
+
+
+#endif
